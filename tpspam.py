6eef0eb856a5341199baf33fd7897fcfab47e61b
YohanFr37 <yohan.boucher.yb@gmail.com>
2023-03-31 01:16:55 +0200

    modif

--- tpspam.py
+++ tpspam.py
@@ -2,16 +2,17 @@ import numpy as np
 import os
 import math
 
+
 def lireMail(fichier, dictionnaire):
 	""" 
 	Lire un fichier et retourner un vecteur de booléens en fonctions du dictionnaire
 	"""
-	f = open(fichier, "r",encoding="ascii", errors="surrogateescape")
+	f = open(fichier, "r", encoding="ascii", errors="surrogateescape")
 	mots = f.read().split(" ")
-	
-	x = [False] * len(dictionnaire) 
-	
-	# à modifier...	
+
+	x = [False] * len(dictionnaire)
+
+	# à modifier...
 	"""
 	for i in range(0,len(mots)-1):
 
@@ -34,10 +35,11 @@ def lireMail(fichier, dictionnaire):
 			#print(str(b) + " "+ str(i))
 	"""
 	for i in range(len(dictionnaire)):
-		if(dictionnaire[i] in list(map(str.upper,mots))):
+		if (dictionnaire[i] in list(map(str.upper, mots))):
 			x[i] = True
 	f.close()
 	return x
+
 
 def charge_dico(fichier):
 	f = open(fichier, "r")
@@ -45,9 +47,9 @@ def charge_dico(fichier):
 
 	indiceMot = 0
 	mots = []
-	for i in range (len(motstempo)):
-		#Si le mot fait 3 lettres ou plus, on le garde
-		if(len(motstempo[i]) >= 3): 
+	for i in range(len(motstempo)):
+		# Si le mot fait 3 lettres ou plus, on le garde
+		if (len(motstempo[i]) >= 3):
 			mots.append(motstempo[i])
 
 	print("Chargé " + str(len(mots)) + " mots dans le dictionnaire")
@@ -55,13 +57,24 @@ def charge_dico(fichier):
 
 	return mots[:-1]
 
+
 def apprendBinomial(dossier, fichiers, dictionnaire):
 	"""
 	Fonction d'apprentissage d'une loi binomiale a partir des fichiers d'un dossier
 	Retourne un vecteur b de paramètres 
 		
 	"""
-	b = 0	# à modifier...
+
+
+	y = []
+	b = []
+
+	for path in range(len(fichiers)):
+		y.append(lireMail(dossier+"/"+str(path)+".txt", dictionnaire))
+		# Calcul des probas
+		b.append(np.sum(y, 1)[path]/len(fichiers))
+		#print(b)
+	#b = 0  # à modifier...
 	return b
 
 
@@ -72,9 +85,10 @@ def prediction(x, Pspam, Pham, bspam, bham):
 		Retourne True ou False.
 		
 	"""
-	
+
 	return False  # à modifier...
-	
+
+
 def test(dossier, isSpam, Pspam, Pham, bspam, bham):
 	"""
 		Test le classifieur de paramètres Pspam, Pham, bspam, bham 
@@ -85,9 +99,8 @@ def test(dossier, isSpam, Pspam, Pham, bspam, bham):
 	"""
 	fichiers = os.listdir(dossier)
 	for fichier in fichiers:
-		print("Mail " + dossier+"/"+fichier)		
+		print("Mail " + dossier+"/"+fichier)
 
-		
 		# à compléter...
 
 	return 0  # à modifier...
@@ -95,7 +108,7 @@ def test(dossier, isSpam, Pspam, Pham, bspam, bham):
 
 ############ programme principal ############
 
-dossier_spams = "spam/baseapp/spam"	# à vérifier
+dossier_spams = "spam/baseapp/spam"  # à vérifier
 dossier_hams = "spam/baseapp/ham"
 
 fichiersspams = os.listdir(dossier_spams)
@@ -106,35 +119,27 @@ mHam = len(fichiershams)
 
 # Chargement du dictionnaire:
 dictionnaire = charge_dico("spam/dictionnaire1000en.txt")
-#print(dictionnaire)
+# print(dictionnaire)
 
 # Apprentissage des bspam et bham:
 print("apprentissage de bspam...")
 bspam = apprendBinomial(dossier_spams, fichiersspams, dictionnaire)
 print("apprentissage de bham...")
 bham = apprendBinomial(dossier_hams, fichiershams, dictionnaire)
-
-initial_count=0
+print(bspam)
+print(bham)
+"""
+initial_count = 0
 # Calcul des probabilités a priori Pspam et Pham:
-
-yspam = []
-Pspam = []
-
-for path in range(len(fichiersspams)):
-	yspam.append(lireMail(dossier_spams+"/"+str(path)+".txt", dictionnaire))
-	#Calcul des probas
-	Pspam.append(np.sum(yspam,1)[path]/len(fichiersspams))
-print(Pspam)
 
 yham = []
 Pham = []
 
 for path in range(len(fichiershams)):
 	yham.append(lireMail(dossier_hams+"/"+str(path)+".txt", dictionnaire))
-	#Calcul des probas
-	Pham.append(np.sum(yham,1)[path]/len(fichiershams))
+	# Calcul des probas
+	Pham.append(np.sum(yham, 1)[path]/len(fichiershams))
 print(Pham)
 
 # Calcul des erreurs avec la fonction test():
-
-
+"""
