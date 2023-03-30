00822b8659fa83b903df3a7f621c9841bc10b238
YohanFr37 <yohan.boucher.yb@gmail.com>
2023-03-31 00:58:51 +0200

    Calcul proba

--- tpspam.py
+++ tpspam.py
@@ -10,17 +10,49 @@ def lireMail(fichier, dictionnaire):
 	mots = f.read().split(" ")
 	
 	x = [False] * len(dictionnaire) 
-
-	# à modifier...
 	
+	# à modifier...	
+	"""
+	for i in range(0,len(mots)-1):
+
+		for j in range(0,len(dictionnaire)-1):
+			if(mots[i].upper() == dictionnaire[j]):
+				print(dictionnaire[j])
+				x[i] = True
+				a+=1
+				#print(str(a) + " "+ str(i))
+				#print("coucou")
+				break
+			#print("mots " + str(mots[i]) + " dico " + str(dictionnaire[j]))
+	print(a)
+	
+	for i in range(0,len(mots)-1):
+		if(mots[i].upper() in dictionnaire):
+
+			x[i] = True
+			b += 1
+			#print(str(b) + " "+ str(i))
+	"""
+	for i in range(len(dictionnaire)):
+		if(dictionnaire[i] in list(map(str.upper,mots))):
+			x[i] = True
 	f.close()
 	return x
 
 def charge_dico(fichier):
 	f = open(fichier, "r")
-	mots = f.read().split("\n")
+	motstempo = f.read().split("\n")
+
+	indiceMot = 0
+	mots = []
+	for i in range (len(motstempo)):
+		#Si le mot fait 3 lettres ou plus, on le garde
+		if(len(motstempo[i]) >= 3): 
+			mots.append(motstempo[i])
+
 	print("Chargé " + str(len(mots)) + " mots dans le dictionnaire")
 	f.close()
+
 	return mots[:-1]
 
 def apprendBinomial(dossier, fichiers, dictionnaire):
@@ -63,8 +95,8 @@ def test(dossier, isSpam, Pspam, Pham, bspam, bham):
 
 ############ programme principal ############
 
-dossier_spams = "spam"	# à vérifier
-dossier_hams = "ham"
+dossier_spams = "spam/baseapp/spam"	# à vérifier
+dossier_hams = "spam/baseapp/ham"
 
 fichiersspams = os.listdir(dossier_spams)
 fichiershams = os.listdir(dossier_hams)
@@ -73,8 +105,8 @@ mSpam = len(fichiersspams)
 mHam = len(fichiershams)
 
 # Chargement du dictionnaire:
-dictionnaire = charge_dico("dictionnaire1000en.txt")
-print(dictionnaire)
+dictionnaire = charge_dico("spam/dictionnaire1000en.txt")
+#print(dictionnaire)
 
 # Apprentissage des bspam et bham:
 print("apprentissage de bspam...")
@@ -82,10 +114,26 @@ bspam = apprendBinomial(dossier_spams, fichiersspams, dictionnaire)
 print("apprentissage de bham...")
 bham = apprendBinomial(dossier_hams, fichiershams, dictionnaire)
 
+initial_count=0
 # Calcul des probabilités a priori Pspam et Pham:
-# Pspam = 
-# Pham = 
 
+yspam = []
+Pspam = []
+
+for path in range(len(fichiersspams)):
+	yspam.append(lireMail(dossier_spams+"/"+str(path)+".txt", dictionnaire))
+	#Calcul des probas
+	Pspam.append(np.sum(yspam,1)[path]/len(fichiersspams))
+print(Pspam)
+
+yham = []
+Pham = []
+
+for path in range(len(fichiershams)):
+	yham.append(lireMail(dossier_hams+"/"+str(path)+".txt", dictionnaire))
+	#Calcul des probas
+	Pham.append(np.sum(yham,1)[path]/len(fichiershams))
+print(Pham)
 
 # Calcul des erreurs avec la fonction test():
 
